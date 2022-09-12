c72e187000854970ce180c5acc6f1df933761b5b
Wynne <76490156+wwyynnee@users.noreply.github.com>
2022-09-12 17:11:44 +0000

    Модификация файлов: вёрстка сайта, улучшение навигации

--- script.js
+++ script.js
@@ -1,10 +1,67 @@
-const copy_email = () => {
-  let copyemail = document.getElementById("email");
-  navigator.clipboard.writeText(copyemail.textContent);
-  swal("Почта скопирована", `${copyemail.textContent}`, "success");
-}
-const copy_tel = () => {
-  let copytel = document.getElementById("tel");
-  navigator.clipboard.writeText(copytel.textContent);
-  swal("Номер телефона скопирован", `${copytel.textContent}`, "success");
-}
+$(".nav__container").on("click", () => {
+  $(".nav__elements").css("border-radius", "4px");
+
+  if (
+    $(".about").on("click", () => {
+      $(".about").css({
+        "background-color": "#ffffff20",
+        "border-bottom": "solid 3px #636bfc",
+        "color": "#636bfc"
+      });
+    })
+  ) {
+    $(".price, .portfolio, .contacts").css({
+      "color": "",
+      "border-bottom": "",
+      "background-color": ""
+    });
+  }
+  
+  if (
+    $(".price").on("click", () => {
+      $(".price").css({
+        "background-color": "#ffffff20",
+        "border-bottom": "solid 3px #636bfc",
+        "color": "#636bfc"
+      });
+    })
+  ) {
+    $(".about, .portfolio, .contacts").css({
+      "color": "",
+      "border-bottom": "",
+      "background-color": ""
+    });
+  }
+  
+  if (
+    $(".portfolio").on("click", () => {
+      $(".portfolio").css({
+        "background-color": "#ffffff20",
+        "border-bottom": "solid 3px #636bfc",
+        "color": "#636bfc"
+      });
+    })
+  ) {
+    $(".about, .price, .contacts").css({
+      "color": "",
+      "border-bottom": "",
+      "background-color": ""
+    });
+  }
+  
+  if (
+    $(".contacts").on("click", () => {
+      $(".contacts").css({
+        "background-color": "#ffffff20",
+        "border-bottom": "solid 3px #636bfc",
+        "color": "#636bfc"
+      });
+    })
+  ) {
+    $(".about, .price, .portfolio").css({
+      "color": "",
+      "border-bottom": "",
+      "background-color": ""
+    });
+  }
+})
